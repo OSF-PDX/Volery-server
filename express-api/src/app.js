1350bcd15f1fb8bceffa66391e5c8c70e96f8ebc
Gregory Propf <greg.osf@nten.org>
2026-03-04 23:55:17 +0000

    Changed URL and port for server.

--- express-api/src/app.js
+++ express-api/src/app.js
@@ -3,7 +3,7 @@ const express = require("express");
 const axios = require("axios");
 
 const app = express();
-const PORT = 3000;
+const PORT = 3001;
 
 const cors = require("cors");
 app.use(cors());
